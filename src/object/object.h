17185dce12c0b8e59738ae1d461aa093044c77cd
Femaref <femaref@googlemail.com>
2013-03-02 19:30:23 +0100

    basic game object structs

---
+++ src/object/object.h
@@ -0,0 +1,10 @@
+#ifndef OBJECT_H_
+#define OBJECT_H_
+
+#include "position.h"
+
+typedef struct {
+  position_type pos;
+} obj_type;
+
+#endif // OBJECT_H_
---
+++ src/object/position.h
@@ -0,0 +1,9 @@
+#ifndef POSITION_H_
+#define POSITION_H_
+
+typedef struct {
+  int x,y,z;
+  int model;
+} position_type;
+
+#endif // POSITION_H_
